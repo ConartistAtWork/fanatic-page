ee058e711f08917cb7b189d4fae36b54f284a27a
ConartistAtWork <mitet101@gmail.com>
2021-07-30 19:43:22 +0300

    mobile menu, minor fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,8 @@
-
-$("a").on("click", function () {
-  $("html, body").animate({ scrollTop: 0 }, "slow");
-  return false;
-});
-
-// function resize() {
-//   let nav = $('nav-group:first-child')
-//   if ($(window).width() < 1048
-//     && x.className !== "mobilemenu show") {
-//     x.className = "mobilemenu"
-//   }
-//   else if (x.className === "mobilemenu show") {
-//     return none
-//   }
-//   else {
-//     x.className = "topmenu"
-//     $(".dropdown-men").attr("class", "dropdown-men hidden")
-//   }
-// }
-
-function saveWidth() {
-  
+function openMenu() {
+  document.getElementById("nav-open").classList.add('open');
+  console.log("Marina is stupid")
 }
 
-let prevWidth = $(window).width()
-
-console.log(prevWidth)
-
-function resize() {
-  /// bgResize Start
-  let widthDiff = prevWidth - $(window).width();
-  let offSet = -20;
-  if (widthDiff == 40) {
-    offSet -= 20;
-    prevWidth = $(window).width()
-  }
-  $('.introduction').css({ 'background-position': "right " + offSet + "px " + "top " + "0" });
-  /// bgResize Finish
-  let nav = $('nav-group:first-child')
-  if ($(window).width() < 1048
-    && nav.className !== "mobilemenu show") {
-    nav.className = "mobilemenu"
-  }
-  prevWidth = $(window).width()
+function closeMenu() {
+  document.getElementById("nav-open").classList.remove('open');
 }
-
-function bgResize() {
-
-  var thisWidth = $(document).width(),
-    offSet = thisWidth -1930;
-
-  if (offSet < -20) {
-    $('.introduction').css({ 'background-position': "right " + offSet + "px " + "top " + "0" });
-  }
-  else
-    $('.introduction').css({ 'background-position': "right " + -10 + "px " + "top " + "0" })
-}
-
-
-// $(window).resize(bgResize);
-// $(document).ready(bgResize);
